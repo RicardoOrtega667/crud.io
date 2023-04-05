7214db28a68828ed745a5ef075fbd6ce1b2eef86
RicardoOrtega667 <ricardo_or93@outlook.com>
2023-04-04 21:12:27 -0600

    segundo commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const getValueInput = () =>{
+    let inputValue1 = document.querySelector("#name").value; 
+    let inputValue2 = document.querySelector("#tarea").value; 
+    document.querySelector("#valueInput").innerHTML = `First input value: ${inputValue1} Second Input Value: ${inputValue2}`; 
+  }
+
+  console.log();
