ba7dd3f6e54c3df56189a6a1eb99c02691eda9ce
RicardoOrtega667 <ricardo_or93@outlook.com>
2023-04-07 12:52:57 -0600

    cuarto commit

--- app.js
+++ app.js
@@ -26,4 +26,53 @@ addButton.addEventListener('click', function() {
   input1.value = '';
   input2.value = '';
   
+  // Actualizar lista
+  updateList();
+});
+
+// Función para actualizar la lista
+function updateList() {
+  // Vaciar la lista
+  list.innerHTML = '';
   
+  // Iterar sobre el arreglo y agregar cada elemento a la lista
+  items.forEach(function(item, index) {
+    const li = document.createElement('li');
+    const text = document.createTextNode(`${item.value1} - ${item.value2}`);
+    
+    // Agregar botones de editar y borrar
+    const editButton = document.createElement('button');
+    editButton.innerHTML = 'Editar';
+    editButton.addEventListener('click', function() {
+      editItem(index);
+    });
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Borrar';
+    deleteButton.addEventListener('click', function() {
+      deleteItem(index);
+    });
+    
+    li.appendChild(text);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+  });
+}
+
+// Función para editar un elemento
+function editItem(index) {
+  // Obtener elemento del arreglo
+  const item = items[index];
+  
+  // Actualizar valores de los inputs
+  input1.value = item.value1;
+  input2.value = item.value2;
+  
+  // Remover elemento del arreglo
+  items.splice(index, 1);
+  
+  // Actualizar lista
+  updateList();
+}
+
