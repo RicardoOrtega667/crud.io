211258d8bc1f356faf6e09ac864cf5ad347e4ea9
RicardoOrtega667 <ricardo_or93@outlook.com>
2023-04-07 11:26:25 -0600

    tercer commit

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+// Crear arreglo vacío
+let items = [];
 
-const getValueInput = () =>{
-    let inputValue1 = document.querySelector("#name").value; 
-    let inputValue2 = document.querySelector("#tarea").value; 
-    document.querySelector("#valueInput").innerHTML = `First input value: ${inputValue1} Second Input Value: ${inputValue2}`; 
-  }
+// Obtener elementos del DOM
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const addButton = document.getElementById('add-button');
+const list = document.getElementById('list');
 
-  console.log();
+// Agregar evento de clic al botón
+addButton.addEventListener('click', function() {
+  // Obtener valores de los inputs
+  const value1 = input1.value;
+  const value2 = input2.value;
+  
+  // Crear objeto con los valores
+  const item = {
+    value1: value1,
+    value2: value2
+  };
+  
+  // Agregar objeto al arreglo
+  items.push(item);
+  
+  // Limpiar inputs
+  input1.value = '';
+  input2.value = '';
+  
+  
