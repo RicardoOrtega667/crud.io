9d783063862014cff40d0d61504c58db10d68962
RicardoOrtega667 <ricardo_or93@outlook.com>
2023-04-07 18:43:00 -0600

    quinto commit

--- app.js
+++ app.js
@@ -2,30 +2,30 @@
 let items = [];
 
 // Obtener elementos del DOM
-const input1 = document.getElementById('input1');
-const input2 = document.getElementById('input2');
-const addButton = document.getElementById('add-button');
-const list = document.getElementById('list');
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const addButton = document.getElementById("add-button");
+const list = document.getElementById("list");
 
 // Agregar evento de clic al botón
-addButton.addEventListener('click', function() {
+addButton.addEventListener("click", function () {
   // Obtener valores de los inputs
   const value1 = input1.value;
   const value2 = input2.value;
-  
+
   // Crear objeto con los valores
   const item = {
     value1: value1,
-    value2: value2
+    value2: value2,
   };
-  
+
   // Agregar objeto al arreglo
   items.push(item);
-  
+
   // Limpiar inputs
-  input1.value = '';
-  input2.value = '';
-  
+  input1.value = "";
+  input2.value = "";
+
   // Actualizar lista
   updateList();
 });
@@ -33,26 +33,26 @@ addButton.addEventListener('click', function() {
 // Función para actualizar la lista
 function updateList() {
   // Vaciar la lista
-  list.innerHTML = '';
-  
+  list.innerHTML = "";
+
   // Iterar sobre el arreglo y agregar cada elemento a la lista
-  items.forEach(function(item, index) {
-    const li = document.createElement('li');
+  items.forEach(function (item, index) {
+    const li = document.createElement("li");
     const text = document.createTextNode(`${item.value1} - ${item.value2}`);
-    
+
     // Agregar botones de editar y borrar
-    const editButton = document.createElement('button');
-    editButton.innerHTML = 'Editar';
-    editButton.addEventListener('click', function() {
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "Editar";
+    editButton.addEventListener("click", function () {
       editItem(index);
     });
-    
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = 'Borrar';
-    deleteButton.addEventListener('click', function() {
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Borrar";
+    deleteButton.addEventListener("click", function () {
       deleteItem(index);
     });
-    
+
     li.appendChild(text);
     li.appendChild(editButton);
     li.appendChild(deleteButton);
@@ -64,15 +64,14 @@ function updateList() {
 function editItem(index) {
   // Obtener elemento del arreglo
   const item = items[index];
-  
+
   // Actualizar valores de los inputs
   input1.value = item.value1;
   input2.value = item.value2;
-  
+
   // Remover elemento del arreglo
   items.splice(index, 1);
-  
+
   // Actualizar lista
   updateList();
 }
-
